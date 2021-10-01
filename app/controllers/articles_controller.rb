e0dbe250ac96c20ac79a4851830f3c4982260cf2
Grant Cloyd <dgcloyd@gmail.com>
2021-09-30 20:05:43 -0500

    add new, create, show html pages, routes, display validations, redirections

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,36 @@
 class ArticlesController < ApplicationController
 
     def show
-    
     @article = Article.find(params[:id])
     # render json: article
 
     end
+
+    def index
+    @articles = Article.all
+    end
+
+    def new
+     @article = Article.new
+    end
+
+    def create
+      @article = Article.new(article_params)
+       if @article.save 
+        flash[:notice] = "Article was created successfully"
+        redirect_to @article
+       else 
+        render 'new'
+      end
+    end
+
+
+    private 
+    
+    def article_params 
+     params.require(:article).permit(:title, :description)
+    end
+
 
 
 end
