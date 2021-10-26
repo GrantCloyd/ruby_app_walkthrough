e0e60995f2ec4b322381708d6ff40f79aacc4b17
Grant Cloyd <dgcloyd@gmail.com>
2021-10-26 15:59:49 -0500

    slight comment to test push to github:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-class User < ApplicationRecord
+class User < ApplicationRecord 
     validates :email, presence: true, uniqueness: true, length: {maximum: 105}, format: {with: URI::MailTo::EMAIL_REGEXP}
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3 ,maximum: 25}
+    has_many :articles
 end
